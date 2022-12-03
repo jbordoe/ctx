03bc3c87d4a2c0c330436a8b2cefe781c825822f
Jesse Bordoe <jesse.bordoe@gmail.com>
2022-12-03 23:23:34 +0100

    use dotenv-parser

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 use clap::Parser;
+use dotenv_parser::parse_dotenv;
 use std::env;
 use std::ffi::OsString;
 use std::process::Command;
@@ -22,15 +23,10 @@ fn main() {
 
     let content = std::fs::read_to_string(&args.env)
         .expect("could not read file");
-    
+
     let cmd = args.command;
     let mut run_cmd = Command::new(&cmd[0]);
-    for line in content.lines() {
-    // TODO: handle comments, validate env var format
-        let split: Vec<&str> = line.split("=").collect();
-        let key = split[0].to_string();
-        let value = split[1].to_string();
-
+    for (key, value) in parse_dotenv(&content).unwrap() {
         run_cmd.env(key, value);
     }
     for arg in &cmd[1..] {
