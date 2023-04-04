ebce25fa1dd6087af0e8566726cc35707cdd3d09
Jesse Bordoe <jesse.bordoe@gmail.com>
2023-04-04 11:59:25 +0200

    add env var aguments

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 
 Fast context switching with predefined environment variable groups
 
-## Example
+## Usage
+
+`ctx -e DOTENV_FILE COMMAND`
+
+Use`ctx` with any `.env` file containing your environment variables.
+
+Consider having multiple `.env` files for different contexts (staging, docker etc) and using `ctx` to easily switch.
+
+### Example
 ```
 ctx -e .env.example printenv | grep FOO
 ```
--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,12 @@ use std::process::Command;
 struct Cli {
     /// path to .env file
     #[arg(short, long)]
-    env: std::path::PathBuf,
+    file: Option<std::path::PathBuf>,
 
+    /// set one or more environment variables
+    #[arg(short, long, value_name = "VAR=VALUE")]
+    env: Vec<String>,
+    
     /// command to run
     command: Vec<OsString>,
 }
@@ -20,15 +24,30 @@ struct Cli {
 /// Inject configured environment variables
 fn main() {
     let args = Cli::parse();
-
-    let content = std::fs::read_to_string(&args.env)
-        .expect("could not read file");
-
     let cmd = args.command;
     let mut run_cmd = Command::new(&cmd[0]);
-    for (key, value) in parse_dotenv(&content).unwrap() {
-        run_cmd.env(key, value);
+
+    if let Some(file_path) = args.file {
+        let content = std::fs::read_to_string(&file_path)
+            .expect("could not read file");
+
+        for (key, value) in parse_dotenv(&content).unwrap() {
+            run_cmd.env(key, value);
+        }
     }
+
+    for env_str in args.env {
+        let parts: Vec<&str> = env_str.split('=').collect();
+        if parts.len() != 2 {
+            println!("Invalid environment variable mapping: {}", env_str);
+            continue;
+        }
+        let var = parts[0];
+        let value = parts[1];
+        run_cmd.env(var, value);
+        println!("Set environment variable {} to {}", var, value);
+    }
+
     for arg in &cmd[1..] {
         run_cmd.arg(arg);
     }
