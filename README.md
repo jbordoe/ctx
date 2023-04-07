63d70e417b402a3d026d89af73d5f76f9a5476a8
Jesse Bordoe <jesse.bordoe@gmail.com>
2023-04-07 17:41:03 +0200

    feat(yml): add yaml support

--- README.md
+++ README.md
@@ -15,6 +15,15 @@ Consider having multiple `.env` files for different contexts (staging, docker et
 ### Examples
 > **_NOTE:_**  In these examples we include our command as a string argument to `bash` to ensure the variables are interpolated **after** Rust updates the environment. In other instances you can just pass your command as is.
 
+#### Using env files
+You can point to any file containing environment variable definitions:
+```bash
+FOO=BAR
+export NAME=ALICE
+# comments will be ignored
+# AGE=99
+```
+
 Run a command using the variables defined in `.env.en.example`
 ```bash
 ctx -f .env.example -- bash -c 'echo "$GREETING, $SUBJECT!"'
@@ -40,6 +49,43 @@ ctx -f .env.de.example -e GREETING=Moin -- bash -c 'echo "$GREETING, $SUBJECT!"'
 Should display
 ```
 Moin, Welt!
+```
+
+#### Using `.ctx.yml`
+You can keep all your variables in a `.ctx.yml` file. The file should contain multiple named `contexts`, each with a list of `variables` which have a `name` and a `value`:
+
+```yaml
+contexts:
+  en:
+    variables:
+      - name: GREETING
+        value: Hello
+      - name: SUBJECT
+        value: World
+
+  it:
+    variables:
+      - name: GREETING
+        value: Ciao
+      - name: SUBJECT
+        value: Mondo
+...
+```
+When you pass in a context, ctx will automatically pull in the variables from `.ctx.yml`:
+```bash
+ctx -c it -- bash -c 'echo "$GREETING, $SUBJECT!"'
+```
+Should display
+```
+Ciao, Mondo!
+```
+You can still use overrides with `-e`:
+```bash
+ctx -c it -e SUBJECT=ragazzi -- bash -c 'echo "$GREETING, $SUBJECT!"'
+```
+Should display
+```
+Ciao, ragazzi!
 ```
 
 Use `ctx --help` for more information
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@
 use clap::Parser;
 use colored::*;
 use dotenv_parser::parse_dotenv;
-use std::env;
-use std::ffi::OsString;
-use std::process::Command;
+use serde::{Deserialize, Serialize};
+use serde_yaml;
+
+use std::{env, ffi::OsString, fs::File, io::Read, process::Command, path::PathBuf};
 
 /// Set preconfigured environment variables and run the given command
 #[derive(Parser)]
 struct Cli {
+    /// context from local .ctx.yml
+    #[arg(short, long)]
+    context: Option<String>,
+
     /// path to .env file
     #[arg(short, long)]
     file: Option<std::path::PathBuf>,
@@ -26,11 +31,76 @@ struct Cli {
     verbose: bool,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Contexts {
+    #[serde(rename = "contexts")]
+    contexts: std::collections::HashMap<String, Context>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Context {
+    variables: Vec<Variable>,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Variable {
+    name: String,
+    value: String,
+}
+
+fn get_context_variables(context: &str) -> Option<Vec<Variable>> {
+    let ctx_file = PathBuf::from(".ctx.yml");
+    let mut file = match File::open(&ctx_file) {
+        Ok(f) => f,
+        Err(_) => return None,
+    };
+
+    let mut contents = String::new();
+    if let Err(_) = file.read_to_string(&mut contents) {
+        return None;
+    }
+
+    let contexts: Contexts = match serde_yaml::from_str(&contents) {
+        Ok(c) => c,
+        Err(_) => return None,
+    };
+
+    match contexts.contexts.get(context) {
+        Some(ctx) => Some(ctx.variables.clone()),
+        None => None,
+    }
+}
+
+
 /// Inject configured environment variables
 fn main() {
     let args = Cli::parse();
     let cmd = args.command;
     let mut run_cmd = Command::new(&cmd[0]);
+
+    if let Some(context) = args.context {
+        if args.verbose {
+            eprintln!(
+                "Reading from context: {}",
+                context.yellow()
+            );
+        }
+        let context_variables = get_context_variables(&context);
+        if let Some(variables) = context_variables {
+            for v in variables.into_iter() {
+                let key = v.name;
+                let value = v.value;
+                if args.verbose {
+                    eprintln!(
+                        "{}={}",
+                        key.bright_yellow().bold(),
+                        value.yellow()
+                    );
+                }
+                run_cmd.env(key, value);
+            }
+        }
+    }
 
     if let Some(file_path) = args.file {
         let content = std::fs::read_to_string(&file_path)
@@ -91,4 +161,5 @@ fn main() {
     run_cmd.spawn()
         .expect("failed to execute command")
         .wait();
+
 }
